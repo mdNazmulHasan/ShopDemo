a11ed03395033dd2a16e5efe48aba817c8abe6aa
mdNazmulHasan <najmul.hasan10@gmail.com>
2020-12-09 21:34:48 +0600

    📦 NEW: loading screen done

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
+import cart from '../../assets/carts.png';
 
 const LoadingScreen = () => {
   return (
-    <View>
-      <Text>Loading Screen</Text>
+    <View style={styles.containerStyle}>
+      <Image source={cart} style={styles.image} />
     </View>
   );
 };
 
 export default LoadingScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+  },
+  containerStyle: {
+    flex: 1,
+    backgroundColor: '#667EEA',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
