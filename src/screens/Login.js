998ccb5513c9c200a24d47016679847ac906142a
mdNazmulHasan <najmul.hasan10@gmail.com>
2020-12-09 22:31:34 +0600

    👌 IMPROVE: removed inline style

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,40 +7,14 @@ import colors from '../utils/colors';
 const Login = () => {
   return (
     <>
-      <SafeAreaView style={{flex: 1, justifyContent: 'space-between'}}>
-        <Text
-          style={{
-            fontSize: 20,
-            alignSelf: 'center',
-            marginVertical: 20,
-          }}>
-          Welcome to Bolt
-        </Text>
-        <Image
-          source={shop}
-          style={{
-            width: 300,
-            height: 300,
-            resizeMode: 'contain',
-            alignSelf: 'center',
-          }}
-        />
+      <SafeAreaView style={styles.containerStyle}>
+        <Text style={styles.welcomeTextStyle}>Welcome to Bolt</Text>
+        <Image source={shop} style={styles.brandImageStyle} />
         <View>
-          <TouchableOpacity
-            style={{
-              backgroundColor: colors.cornFlowerBlue,
-              alignSelf: 'center',
-              paddingHorizontal: 20,
-              paddingVertical: 10,
-              borderRadius: 6,
-            }}>
-            <Text style={{fontSize: 15, color: colors.white}}>
-              Login with phone
-            </Text>
+          <TouchableOpacity style={styles.loginBtnContainerStyle}>
+            <Text style={styles.loginBtnTextStyle}>Login with phone</Text>
           </TouchableOpacity>
-          <Text style={{alignSelf: 'center', padding: 20, fontSize: 18}}>
-            Shop with us
-          </Text>
+          <Text style={styles.shopTextStyle}>Shop with us</Text>
         </View>
       </SafeAreaView>
     </>
@@ -49,4 +23,26 @@ const Login = () => {
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  containerStyle: {flex: 1, justifyContent: 'space-between'},
+  welcomeTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    marginVertical: 20,
+  },
+  brandImageStyle: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+  loginBtnContainerStyle: {
+    backgroundColor: colors.cornFlowerBlue,
+    alignSelf: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 6,
+  },
+  loginBtnTextStyle: {fontSize: 15, color: colors.white},
+  shopTextStyle: {alignSelf: 'center', padding: 20, fontSize: 18},
+});
